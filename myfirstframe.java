2f4b628078e474125603a496fbc1d66beba77441
tracymtrs-sys <tracymtrs@gmail.com>
2026-08-13 21:51:43 +0200

    Add files via upload

---
+++ myfirstframe.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trysee;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author TRY SEE
+ */
+public class myfirstframe extends JFrame {
+    
+    public myfirstframe(){
+        setTitle("my first GUI");
+        setSize(200,300);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setVisible(true);
+        setForeground(Color.blue);
+        
+}
+    
+}
